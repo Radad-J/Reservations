c0b2407e00897049737687781fbc7cf52b23ec23
Radad-J <radad1998@gmail.com>
2021-02-10 21:56:49 +0100

    Type templates + routes

---
+++ resources/views/type/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('title', 'Liste des types')
+@section('content')
+    <h1>Liste des {{ $resource }}</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Type</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($types as $type)
+                <tr>
+                    <td>{{ $type->type }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\TypeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,7 +22,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//Artist routes
 Route::get('artist', [ArtistController::class, 'index'])->name('index');
 Route::get('artist/{id}', [ArtistController::class, 'show'])->name('show');
+
+//Type routes
+Route::get('type', [TypeController::class, 'index'])->name('index');
+Route::get('type/{id}', [TypeController::class, 'show'])->name('show');
 
 
