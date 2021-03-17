44c977792517413015c6909961a08b2b3e69e29c
Radad-J <radad1998@gmail.com>
2021-03-17 17:20:50 +0100

    Update show locality template

--- resources/views/locality/show.blade.php
+++ resources/views/locality/show.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 
-@section('title', 'Fiche d\'une localité')
+@section('title', 'Liste des localités')
 
 @section('content')
     <h1>{{ $locality->postal_code }} {{ $locality->locality }}</h1>
+
+    <ul>
+        @foreach($locality->locations as $location)
+            <li>{{ $location->designation }}</li>
+        @endforeach
+    </ul>
 @endsection
