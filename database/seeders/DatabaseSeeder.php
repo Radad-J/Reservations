d78e80b9eb0fb62518c493086829937c77bb1979
Radad-J <radad1998@gmail.com>
2021-03-28 15:40:32 +0200

    Add seeders to ArtistType + deactivate constraints in Artist and Type

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             LocationsTableSeeder::class,
             ShowsTableSeeder::class,
             RepresentationsTableSeeder::class,
+            ArtistTypeTableSeeder::class,
         ]);
 
     }
