4d50667b0e7469c001ae2049a06b3be6fa6abc2b
Radad-J <radad1998@gmail.com>
2021-05-31 20:50:09 +0200

    Delete firstname/lastname and login seeds

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -20,32 +20,23 @@ class UsersTableSeeder extends Seeder
 
         $users = [
             [
-                'login' => 'bob123',
+                'name' => 'Bob',
                 'password' => '$2y$10$O8TR5s3ouefCXXR9lAyetuT0i2cpuG3z0uxSk1e3rAIWnLnJJIVf2', // 12345678
-                'firstname' => 'bob',
-                'lastname' => 'sull',
                 'email' => 'bob@sull.com',
-                'language' => 'fr',
             ],
             [
-                'login' => 'john123',
+                'name' => 'John',
                 'password' => '$2y$10$O8TR5s3ouefCXXR9lAyetuT0i2cpuG3z0uxSk1e3rAIWnLnJJIVf2',
-                'firstname' => 'john',
-                'lastname' => 'doe',
                 'email' => 'john@doe.com',
-                'language' => 'en',
             ],
         ];
 
         foreach ($users as $user) {
 
             DB::table('users')->insert([
-                'login' => $user['login'],
+                'name' => $user['name'],
                 'password' => $user['password'],
-                'firstname' => $user['firstname'],
-                'lastname' => $user['lastname'],
                 'email' => $user['email'],
-                'language' => $user['language'],
             ]);
         }
     }
