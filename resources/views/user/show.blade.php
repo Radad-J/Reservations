11cfe65edc86f9912f435217fb9f06f8312ac292
Radad-J <radad1998@gmail.com>
2021-06-01 22:01:11 +0200

    Update User show Template

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -7,21 +7,23 @@
 <div class="card border-primary mt-3 mb-3 mx-auto" style="min-width: 20rem;">
     <div class="card-body">
         <h4 class="card-title">{{ ucfirst($user->firstname) }} {{ ucfirst($user->lastname) }}</h4>
+        <tr>{{menu('User')}}</tr>
         <table class="table table-hover">
             <thead>
                 <tr>
                     <th scope="col">E-mail</th>
-                    <th scope="col">Login</th>
-                    <th scope="col">Firstname</th>
-                    <th scope="col">Lastname</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Last modified: </th>
+
                 </tr>
             </thead>
             <tbody>
                 <tr class="table-active">
                     <td>{{ $user->email }}</td>
-                    <td>{{ $user->login }}</td>
-                    <td>{{ $user->firstname }}</td>
-                    <td>{{ $user->lastname }}</td>
+                    <td>{{ $user->name }}</td>
+                    @if($user->updated_at)
+                        <td>{{ $user->updated_at }}</td>
+                    @endif
                 </tr>
             </tbody>
         </table>
