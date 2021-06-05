682c63e6a72f4127beb815917a1a3fdfa48fdfdc
Nathan D <nathandeltour2@gmail.com>
2021-06-05 18:01:51 +0200

    Translated texts to english & added order details (amount of seats)

--- resources/views/show/show.blade.php
+++ resources/views/show/show.blade.php
@@ -8,7 +8,7 @@
         <h1>{{ $show->title }}</h1>
 
         @if($show->poster_url)
-            <p><img src="{{ asset('/storage/'.$show->poster_url) }}" alt="{{ $show->title }}" width="200"></p>
+            <p><img src="{{ asset('/storage/'. $show->poster_url) }}" alt="{{ $show->title }}" width="200"></p>
         @else
             <canvas width="200" height="100" style="border:1px solid #000000;"></canvas>
         @endif
@@ -18,28 +18,12 @@
         @endif
 
         @if($show->location)
-            <p><strong>Lieu de diffusion :</strong> {{ $show->location->designation }}</p>
+            <p><strong>Theatre :</strong> {{ $show->location->designation }}</p>
         @endif
 
-        <p><strong>Prix :</strong> {{ $show->price }} €</p>
+        <p><strong>Price :</strong> {{ $show->price }} €</p>
 
-        @if($show->bookable && $show->representations->count() > 0)
-            <form action="{{ route('cart.store') }}" method="post">
-                @csrf
-                <input type="hidden" name="show_id" value="{{ $show->id }}">
-                <label for="representation"></label>
-                <select name="representation_id" id="representation_id">
-                    @foreach($show->representations as $representation)
-                        <option value="{{ $representation->id }}">{{ $representation->when }}</option>
-                    @endforeach
-                </select>
-                <button type="submit" class="btn btn-outline-success">Réserver une place</button>
-            </form>
-        @else
-            <p><em>Non réservable</em></p>
-        @endif
-
-        <h2>Liste des représentations</h2>
+        <h4>Representations</h4>
         @if($show->representations->count()>=1)
             <ul>
                 @foreach ($show->representations as $representation)
@@ -47,11 +31,11 @@
                 @endforeach
             </ul>
         @else
-            <p>Aucune représentation</p>
+            <p>No representation</p>
         @endif
 
-        <h2>Liste des artistes</h2>
-        <p><strong>Auteur:</strong>
+        <h4>Artists</h4>
+        <p>
             @foreach ($collaborateurs['auteur'] as $auteur)
                 {{ $auteur->firstname }}
                 {{ $auteur->lastname }}
@@ -60,22 +44,66 @@
             @endforeach
         </p>
 
-        <p><strong>Metteur en scène:</strong>
+        <h4>Directors</h4>
+        <p>
             @foreach ($collaborateurs['scénographe'] as $scenographe)
                 {{ $scenographe->firstname }}
                 {{ $scenographe->lastname }}
-                @if($loop->iteration == $loop->count-1) et
+                @if($loop->iteration === $loop->count-1) and
                 @elseif(!$loop->last), @endif
             @endforeach
         </p>
 
-        <p><strong>Distribution:</strong>
+        <h4>Distribution</h4>
+        <p>
             @foreach ($collaborateurs['comédien'] as $comedien)
                 {{ $comedien->firstname }}
                 {{ $comedien->lastname }}
-                @if($loop->iteration === $loop->count-1) et
+                @if($loop->iteration === $loop->count-1) and
                 @elseif(!$loop->last), @endif
             @endforeach
         </p>
 
+        <h2 class="mt-2 font-weight-500">Buy your ticket now !</h2>
+        @if($show->bookable && $show->representations->count() > 0)
+            <form action="{{ route('cart.store') }}" method="post">
+                @csrf
+                <input type="hidden" name="show_id" value="{{ $show->id }}">
+
+                <!-- Theatre -->
+                <div class="mb-2">
+                    <h5 class="font-weight-bold">Theatre</h5>
+                    <p> {{ $show->location->designation }}</p>
+                </div>
+
+                <!-- Representation select -->
+                <div class="mb-2">
+                    <h5 class="font-weight-bold">
+                        <label for="representation_id">Select a representation</label>
+                    </h5>
+
+                    <select name="representation_id" id="representation_id">
+                        @foreach($show->representations as $representation)
+                            <option value="{{ $representation->id }}">{{ $representation->when }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <!-- Quantity -->
+                <div class="mb-2">
+                    <h5 class="font-weight-bold">
+                        <label for="order_quantity">Enter the amount of seats desired</label>
+                    </h5>
+
+                    <input type="number" max="30" min="1" value="1" name="order_quantity" id="order_quantity">
+                    <span> seat(s)</span>
+                </div>
+
+                <!-- Submit button -->
+                <button type="submit" class="btn btn-outline-success mb-5 mt-2">Book your order</button>
+            </form>
+        @else
+            <p>This show is not bookable at this moment.</p>
+        @endif
+    </article>
 @endsection
