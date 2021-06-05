0e907386204f65efc25e6e8803dfa9eed3dc53f0
Nathan D <nathandeltour2@gmail.com>
2021-06-05 17:19:59 +0200

    Added methods to retrieve representation's info

--- app/Models/Representation.php
+++ app/Models/Representation.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
 class Representation extends Model
 {
@@ -31,5 +34,37 @@ class Representation extends Model
 
     public function users () {
         return $this->belongsToMany(User::class);
+    }
+
+    /**
+     * getRepresentationInfo method.
+     * Retrieves infos of a representation
+     * @param int $id The representation's id
+     * @return Collection
+     */
+    public static function getRepresentationInfo(int $id): Collection
+    {
+        return DB::table('representations')
+            ->join('locations', 'location_id', '=', 'locations.id')
+            ->select('representations.when', 'locations.designation')
+            ->where('representations.id', '=', $id)
+            ->get();
+    }
+
+    /**
+     * getDateAndTime method.
+     * Formats the date & time of a representation
+     * @param string $str
+     * @return array
+     */
+    public static function getDateAndTime(string $str): array
+    {
+        $date = Carbon::parse($str)->format('d/m/Y');
+        $time = Carbon::parse($str)->format('G:i');
+
+        return [
+            'date' => $date,
+            'time' => $time
+        ];
     }
 }
