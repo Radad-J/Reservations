18e5159dfabeda6b84ebf3cb6af2c90cb1d6bd15
Nathan D <nathandeltour2@gmail.com>
2021-06-06 17:23:17 +0200

    Added missing cells + forced import of show's artists

--- app/Imports/ShowsImport.php
+++ app/Imports/ShowsImport.php
@@ -2,22 +2,31 @@
 
 namespace App\Imports;
 
+use App\Models\Artist;
 use App\Models\Show;
+use Illuminate\Support\Str;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
 class ShowsImport implements ToModel, WithHeadingRow
 {
-    public function model(array $row): Show
+    public function model(array $row)
     {
-        $slug = str_replace(' ', '-', $row['title']);
-
-        return new Show([
+        $show = new Show([
             'title'       => $row['title'],
-            'slug'        => $slug,
+            'slug'        => Str::slug($row['slug']),
             'description' => $row['description'],
-            'price'       => $row['price']
+            'poster_url'  => $row['poster_url'],
+            'bookable'    => $row['bookable'],
+            'location_id' => $row['location_id'],
+            'artists'     => $row['artists'],
+            'price'       => $row['price'],
         ]);
+
+        Show::setShowArtist($row['artists'], $row['title']);
+
+
+        return $show;
     }
 
 }
