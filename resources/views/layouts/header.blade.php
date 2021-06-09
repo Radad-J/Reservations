bf010332051963c8f1b47ba403a3d91ebbba9ee7
Radad-J <radad1998@gmail.com>
2021-06-09 19:50:34 +0200

    Add administration link to the dashboard

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,4 @@
 <nav class="navbar navbar-expand-lg navbar-dark">
-
-
     <div class="collapse navbar-collapse" id="navbarColor01">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
@@ -20,12 +18,18 @@
                     <a class="dropdown-item" href="{{route('locality.index')}}">Localities</a>
                     <a class="dropdown-item" href="{{route('artist.index')}}">Artists</a>
                     @if (Auth::check() && Auth::user()->role->id === 1)
-                        <a class="dropdown-item" href="{{ route('show.excel') }}">Download shows list as an Excel file</a>
+                        <a class="dropdown-item" href="{{ route('show.excel') }}">Download shows list as an Excel
+                            file</a>
                         <a class="dropdown-item" href="{{ route('show.csv') }}">Download shows list as a CSV file</a>
                         <a class="dropdown-item" href="{{ route('show.import') }}">Import the shows</a>
                     @endif
                 </div>
             </li>
+            @if (Auth::check() && Auth::user()->role->id === 1)
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('administration')}}">Administration</a>
+                </li>
+            @endif
         </ul>
         <ul class="navbar-nav ml-auto">
             @if(Auth::user())
