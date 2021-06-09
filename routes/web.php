163896d9a945261bb8bd5cc3628e79f4bee5b3b1
Radad-J <radad1998@gmail.com>
2021-06-09 16:38:59 +0200

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use TCG\Voyager\Voyager;
 
 /*
 |--------------------------------------------------------------------------
