8b526903c3622de4ddc43cb891e7f79271cbe2f0
Radad-J <radad1998@gmail.com>
2021-06-11 16:24:30 +0200

    Update show.show design

--- resources/views/show/show.blade.php
+++ resources/views/show/show.blade.php
@@ -4,109 +4,119 @@
 
 @section('content')
     <article>
+        <h1 class="my-5">{{ $show->title }}</h1>
+        <div class="row">
+            <div class="col-4">
+                @if($show->poster_url)
+                    <p><img src="{{ asset('/storage/'. $show->poster_url) }}" alt="{{ $show->title }}" width="250"></p>
+                @else
+                    <canvas width="200" height="100" style="border:1px solid #000000;"></canvas>
+                @endif
+            </div>
+            <div class="col-8">
+                @if($show->description)
+                    <p><strong>Description :</strong> {{ $show->description }}</p>
+                @endif
 
-        <h1>{{ $show->title }}</h1>
+                @if($show->location)
+                    <p><strong>Theatre :</strong> {{ $show->location->designation }}</p>
+                @endif
 
-        @if($show->poster_url)
-            <p><img src="{{ asset('/storage/'. $show->poster_url) }}" alt="{{ $show->title }}" width="200"></p>
-        @else
-            <canvas width="200" height="100" style="border:1px solid #000000;"></canvas>
-        @endif
+                <p><strong>Price :</strong> {{ $show->price }} €</p>
 
-        @if($show->description)
-            <p><strong>Description :</strong> {{ $show->description }}</p>
-        @endif
-
-        @if($show->location)
-            <p><strong>Theatre :</strong> {{ $show->location->designation }}</p>
-        @endif
-
-        <p><strong>Price :</strong> {{ $show->price }} €</p>
-
-        <h4>Representations</h4>
-        @if($show->representations->count()>=1)
-            <ul>
-                @foreach ($show->representations as $representation)
-                    <li>{{ $representation->when }}</li>
-                @endforeach
-            </ul>
-        @else
-            <p>No representation</p>
-        @endif
-        @if(isset($collaborateurs['auteur']))
-            <h4>Artists</h4>
-            <p>
-                @foreach ($collaborateurs['auteur'] as $auteur)
-                    {{ $auteur->firstname }}
-                    {{ $auteur->lastname }}
-                    @if($loop->iteration === $loop->count-1) et
-                    @elseif(!$loop->last), @endif
-                @endforeach
-            </p>
-        @endif
-        @if(isset($collaborateurs['scénographe']))
-            <h4>Directors</h4>
-            <p>
-                @foreach ($collaborateurs['scénographe'] as $scenographe)
-                    {{ $scenographe->firstname }}
-                    {{ $scenographe->lastname }}
-                    @if($loop->iteration === $loop->count-1) and
-                    @elseif(!$loop->last), @endif
-                @endforeach
-            </p>
-        @endif
-
-        @if(isset($collaborateurs['comédien']))
-            <h4>Distribution</h4>
-            <p>
-                @foreach ($collaborateurs['comédien'] as $comedien)
-                    {{ $comedien->firstname }}
-                    {{ $comedien->lastname }}
-                    @if($loop->iteration === $loop->count-1) and
-                    @elseif(!$loop->last), @endif
-                @endforeach
-            </p>
-        @endif
-        <h2 class="mt-2 font-weight-500">Buy your ticket now !</h2>
-        @if($show->bookable && $show->representations->count() > 0)
-            <form action="{{ route('cart.store') }}" method="post">
-                @csrf
-                <input type="hidden" name="show_id" value="{{ $show->id }}">
-
-                <!-- Theatre -->
-                <div class="mb-2">
-                    <h5 class="font-weight-bold">Theatre</h5>
-                    <p> {{ $show->location->designation }}</p>
-                </div>
-
-                <!-- Representation select -->
-                <div class="mb-2">
-                    <h5 class="font-weight-bold">
-                        <label for="representation_id">Select a representation</label>
-                    </h5>
-
-                    <select name="representation_id" id="representation_id">
-                        @foreach($show->representations as $representation)
-                            <option value="{{ $representation->id }}">{{ $representation->when }}</option>
+                <h4>Representations :</h4>
+                @if($show->representations->count()>=1)
+                    <ul style="list-style: none">
+                        @foreach ($show->representations as $representation)
+                            <li>- {{ $representation->when }}</li>
                         @endforeach
-                    </select>
+                    </ul>
+                @else
+                    <p>No representation</p>
+                @endif
+                @if(isset($collaborateurs['auteur']))
+                    <h4>Artists :</h4>
+                    <p>
+                        @foreach ($collaborateurs['auteur'] as $auteur)
+                            {{ $auteur->firstname }}
+                            {{ $auteur->lastname }}
+                            @if($loop->iteration === $loop->count-1) et
+                            @elseif(!$loop->last), @endif
+                        @endforeach
+                    </p>
+                @endif
+                @if(isset($collaborateurs['scénographe']))
+                    <h4>Directors :</h4>
+                    <p>
+                        @foreach ($collaborateurs['scénographe'] as $scenographe)
+                            {{ $scenographe->firstname }}
+                            {{ $scenographe->lastname }}
+                            @if($loop->iteration === $loop->count-1) and
+                            @elseif(!$loop->last), @endif
+                        @endforeach
+                    </p>
+                @endif
+
+                @if(isset($collaborateurs['comédien']))
+                    <h4>Distribution :</h4>
+                    <p>
+                        @foreach ($collaborateurs['comédien'] as $comedien)
+                            {{ $comedien->firstname }}
+                            {{ $comedien->lastname }}
+                            @if($loop->iteration === $loop->count-1) and
+                            @elseif(!$loop->last), @endif
+                        @endforeach
+                    </p>
+                @endif
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <hr style="border: none;border-top: 3px double #333;color: #333;overflow: visible;text-align: center;height: 5px;">
+                    <h2 class="my-4 font-weight-500">Buy your ticket now !</h2>
+                    @if($show->bookable && $show->representations->count() > 0)
+                        <form action="{{ route('cart.store') }}" method="post">
+                            @csrf
+                            <input type="hidden" name="show_id" value="{{ $show->id }}">
+
+                            <!-- Theatre -->
+                            <div class="mb-2">
+                                <h5 class="font-weight-bold">Theatre</h5>
+                                <p> {{ $show->location->designation }}</p>
+                            </div>
+
+                            <!-- Representation select -->
+                            <div class="mb-2">
+                                <h5 class="font-weight-bold">
+                                    <label for="representation_id">Select a representation</label>
+                                </h5>
+
+                                <select name="representation_id" id="representation_id">
+                                    @foreach($show->representations as $representation)
+                                        <option value="{{ $representation->id }}">{{ $representation->when }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <!-- Quantity -->
+                            <div class="mb-2">
+                                <h5 class="font-weight-bold">
+                                    <label for="order_quantity">Enter the amount of seats desired</label>
+                                </h5>
+
+                                <input type="number" max="30" min="1" value="1" name="order_quantity"
+                                       id="order_quantity">
+                                <span> seat(s)</span>
+                            </div>
+
+                            <!-- Submit button -->
+                            <button type="submit" class="btn btn-outline-success mb-5 mt-2">Book your order</button>
+                        </form>
+                    @else
+                        <p>This show is not bookable at this moment.</p>
+                    @endif
                 </div>
-
-                <!-- Quantity -->
-                <div class="mb-2">
-                    <h5 class="font-weight-bold">
-                        <label for="order_quantity">Enter the amount of seats desired</label>
-                    </h5>
-
-                    <input type="number" max="30" min="1" value="1" name="order_quantity" id="order_quantity">
-                    <span> seat(s)</span>
-                </div>
-
-                <!-- Submit button -->
-                <button type="submit" class="btn btn-outline-success mb-5 mt-2">Book your order</button>
-            </form>
-        @else
-            <p>This show is not bookable at this moment.</p>
-        @endif
+            </div>
+        </div>
     </article>
 @endsection
